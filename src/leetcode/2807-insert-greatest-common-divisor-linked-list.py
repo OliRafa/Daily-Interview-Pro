0ef14af0b09c25314fd92fc0d6622df4c42860fd
Rafael Augusto de Oliveira <olirafa@proton.me>
2025-05-27 09:09:25 +0000

    feat(leetcode): add resolution for insert greatest common divisords in linked list

---
+++ src/leetcode/2807-insert-greatest-common-divisor-linked-list.py
@@ -0,0 +1,107 @@
+# 2807. Insert Greatest Common Divisors in Linked List
+
+# Given the head of a linked list head, in which each node contains an integer value.
+
+# Between every pair of adjacent nodes, insert a new node with a value equal to the greatest
+# common divisor of them.
+
+# Return the linked list after insertion.
+
+# The greatest common divisor of two numbers is the largest positive integer that evenly
+# divides both numbers.
+
+# Example 1:
+
+# https://assets.leetcode.com/uploads/2023/07/18/ex1_copy.png
+# Input: head = [18,6,10,3]
+# Output: [18,6,6,2,10,1,3]
+
+# Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes (nodes in blue are the inserted nodes).
+# - We insert the greatest common divisor of 18 and 6 = 6 between the 1st and the 2nd nodes.
+# - We insert the greatest common divisor of 6 and 10 = 2 between the 2nd and the 3rd nodes.
+# - We insert the greatest common divisor of 10 and 3 = 1 between the 3rd and the 4th nodes.
+# There are no more adjacent nodes, so we return the linked list.
+
+# Example 2:
+
+# https://assets.leetcode.com/uploads/2023/07/18/ex2_copy1.png
+# Input: head = [7]
+# Output: [7]
+
+# Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes.
+
+# There are no pairs of adjacent nodes, so we return the initial linked list.
+
+# Constraints:
+# The number of nodes in the list is in the range [1, 5000].
+# 1 <= Node.val <= 1000
+
+
+class ListNode:
+    def __init__(self, value=0, next: "ListNode" = None) -> None:
+        self.value = value
+        self.next = next
+
+
+def build_list(values: list[int]) -> ListNode:
+    head = ListNode()
+    next = None
+    for value in values:
+        if next is None:
+            head = ListNode(value)
+            next = head
+
+        else:
+            next.next = ListNode(value)
+            next = next.next
+
+    return head
+
+
+def to_list(head) -> list[int]:
+    values = []
+    while head.next:
+        values.append(head.value)
+        head = head.next
+
+    values.append(head.value)
+
+    return values
+
+
+def _calculate_greatest_common_divisor(a, b) -> int:
+    while b != 0:
+        temp = a % b
+        a = b
+        b = temp
+
+    return a
+
+
+def insert_greatest_common_divisors(head: ListNode) -> ListNode:
+    current = head
+
+    while current.next:
+        next = current.next
+        divisor = _calculate_greatest_common_divisor(current.value, next.value)
+        current.next = ListNode(divisor, next)
+        current = current.next.next
+
+    return head
+
+
+input = [18, 6, 10, 3]
+output = [18, 6, 6, 2, 10, 1, 3]
+
+result = insert_greatest_common_divisors(build_list(input))
+result = to_list(result)
+
+assert result == output, result
+
+input = [7]
+output = [7]
+
+result = insert_greatest_common_divisors(build_list(input))
+result = to_list(result)
+
+assert result == output, result
